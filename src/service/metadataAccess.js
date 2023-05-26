5a7d243fbb27498fe1f513f3bf3c27301509dd31
Aferpal <fernandezpalomoabel@gmail.com>
2023-05-26 18:52:58 +0200

    si

--- src/service/metadataAccess.js
+++ src/service/metadataAccess.js
@@ -3,17 +3,15 @@ import {Buffer} from "node:buffer"
 import { error } from "node:console";
 
 export function accessImage(path){
-  console.log("has pasado x aqui");
   return new Promise((resolve, reject) =>{
     jsmediatags.read(path, {
       onSuccess: function(metadata){
         if(metadata.tags.picture){
-          console.log(metadata.tags)
           let byteBuffer = metadata.tags.picture.data??"";
           let base64String = Buffer(byteBuffer)
           resolve({base64String: base64String, format: metadata.tags.picture.format});
         }else{
-          reject(new Error(`${path} file has no picture data`));
+          
         }
         
       }, 
